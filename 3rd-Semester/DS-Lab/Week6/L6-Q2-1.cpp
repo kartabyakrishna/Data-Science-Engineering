4900972023b33a3ce8f16308b353c7520a239a8a
kartabyakrishna <kartabyakrishna@gmail.com>
2023-10-25 20:37:14 +0530

    Updated code

--- 3rd-Semester/DS-Lab/Week6/L6-Q2-1.cpp
+++ 3rd-Semester/DS-Lab/Week6/L6-Q2-1.cpp
@@ -1,92 +1,97 @@
-#include<iostream>
+#include<iostream>      // Include the C++ input/output library.
 #include<locale>
-#include<string.h>
-#include<math.h>
-#include<cstdlib>
-using namespace std;
-int stack[100];
-int top = -1;
-int n=100;
-bool isdigt(char x){
+#include<string.h>      // Include the C library for string manipulation.
+#include<math.h>        // Include the C math library.
+#include<cstdlib>       // Include the C standard library.
+using namespace std;     // Use the standard namespace.
+
+int stack[100];           // Declare an integer array for the stack with a capacity of 100 elements.
+int top = -1;              // Initialize the top of the stack to -1 (empty).
+int n = 100;               // Define the maximum capacity of the stack as 100.
+
+// Function to check if a character is a digit.
+bool isdigt(char x) {
    if(x>='0' && x<='9')
-   return true;
+      return true;
    else
-   return false;
+      return false;
 }
 
-void push(int x)
-{
+// Function to push an integer onto the stack.
+void push(int x) {
     if(top>=n-1)
-		cout<<"stack overflow";
+		cout<<"stack overflow";  // If the stack is full, print "stack overflow."
 	else{
-		stack[++top] = x;
+		stack[++top] = x;  // Increment the top index and add the integer to the stack.
 	}
 }
 
-int pop()
-{
+// Function to pop an integer from the stack.
+int pop() {
     if(top == -1)
-        return -1;
+        return -1;   // If the stack is empty, return -1.
     else
-        return stack[top--];
+        return stack[top--];  // Return the integer at the top of the stack and decrement the top index.
 }
+
+// Function to check if a character is an operator.
 int isoperator(char a){
 	if(a=='+'|| a=='-'|| a=='*'|| a=='/'||a=='^')
-	return 1;
+	    return 1;
 	else
-	return 0;
+	    return 0;
 }
+
 int main(){
-	char exp[100],str[100];
+	char exp[100], str[100];
 	double result;
-	int k=0;
+	int k = 0;
 	char op[50];
-	cout<<"enter prefix expression: ";
-	gets(exp);
-	int i=strlen(exp);
+	cout << "enter prefix expression: ";  // Prompt the user to enter a prefix expression.
+	gets(exp);                              // Read the input expression from the user.
+	int i = strlen(exp);
 	for(;i>=0;i--){
 		if(isdigit(exp[i])){
-			k=0;
+			k = 0;
 			while(isdigit(exp[i]))
-			op[k++]=exp[i--];
-			i+=1;
-			op[k]='\0';
-            int j=0;
-			for(int z=strlen(op)-1;z>=0;z--){
-             str[j++]= op[z];
+				op[k++] = exp[i--]; // Collect all contiguous digits.
+			i += 1;
+			op[k] = '\0';
+			int j = 0;
+			for(int z = strlen(op) - 1; z >= 0; z--){
+            	str[j++] = op[z];  // Reverse the digit sequence.
             }
-            string newstr = str;
-			int num = atoi(str);
-			push(num);
+            string newstr = str;  // Convert the reversed string to a C++ string.
+			int num = atoi(str);  // Convert the string to an integer.
+			push(num);           // Push the integer onto the stack.
 		}
-		
 		else if (isoperator(exp[i])){
 			int op1 = pop();
 			int op2 = pop();
-		 switch (exp[i]){
-		 	case '+':
-		 		result =op1+op2 ;
-		 		push(result);
-		 		break;
-		 	case '-':
-		 		result =op1-op2 ;
-		 		push(result);
-		 		break;	
-		 	case '*':
-		 		result =op1*op2 ;
-		 		push(result);
-		 		break;
-		 	case '/':
-		 		result =op1/op2;
-		 		push(result);
-		 		break;
-		 	case '^':
-		 		result = pow(op1,op2) ;
-		 		push(result);
-		 		break;
-		 }
+			switch (exp[i]){
+				case '+':
+					result = op1 + op2 ;
+					push(result);
+					break;
+				case '-':
+					result = op1 - op2 ;
+					push(result);
+					break;	
+				case '*':
+					result = op1 * op2 ;
+					push(result);
+					break;
+				case '/':
+					result = op1 / op2;
+					push(result);
+					break;
+				case '^':
+					result = pow(op1, op2) ;
+					push(result);
+					break;
+			}
 		}
 	}
-	cout<<pop();
+	cout << pop();  // Print the final result.
 	return 0;
 }
